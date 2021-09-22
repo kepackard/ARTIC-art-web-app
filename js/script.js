91b79831c1394c07b7eff9d9fdfa0c22963718a0
Katie Packard-Henderson <kepackard@gmail.com>
2021-09-22 13:15:03 -0400

    Got API to pull to all fields

--- js/script.js
+++ js/script.js
@@ -21,10 +21,11 @@ $(function(){
     const $medium = $('#Medium');
     const $form = $('form');
     const $input = $('input[type="text"]');
+    // const $image = $('IMAGE HERE')
  
 // EVENT LISTENER
 
-    $('form').on('click', getArtData);
+    $form.on('submit', getArtData);
 
 // FUNCTIONS
 
@@ -32,42 +33,39 @@ $(function(){
     // init();
 
     // function init () {
-    //     getLaunchData(); //load launch data
     //     $year.text(new Date().getFullYear)
  
 // Grabbing data from the api
     function getArtData(event) {
         event.preventDefault();
         // Resets search field to an empty string each time. ok to comment-out
-        // const artTitle = $input.val();
+        const artTitle = $input.val();
          $input.val("");
     
-         $.ajax(`${BASE_URL}/artworks/?fields=title,artist_display,date_display,medium_display`).then(function(results) {
+         $.ajax(`${BASE_URL}/artworks/search?q=${artTitle}&fields=title,artist_title,date_display,medium_display,image_id`).then(function(results) {
+          console.log(results);
+          
           artData = results.data;
           console.log("Ajax Call", artData);
 
-        // //save the data locally
-        //   console.log(artData.data[0]);
-    
+     
         render ();
         //transfer data to the DOM
     }, function(error) {
-        console.log(error);
+        console.log("Error", error);
     });
 }
 
 // Access values inside api object
 function render() {
-    const htmlContainer = artData.map((art) => { 
-        $title.text(art.title);
-        $artist.text(art.artist_display);
-        $date.text(art.date_display);
-        $medium.text(art.medium_display);
+    $title.text(artData[0].title);
+    $artist.text(artData[0].artist_title);
+    $date.text(artData[0].date_display);
+    $medium.text(artData[0].medium_display);
 
 //    console.log(artData.data.medium_display);
-    });
+    };
 
-}
 
 // If there is more than one result, pull only the first one
 // https://api.artic.edu/api/v1/artworks?limit=2
@@ -79,6 +77,13 @@ function render() {
 //     "title": "American Gothic",
 //     "image_id": ""
 
+// https://www.artic.edu/iiif/2",
+
 // If the image is not in the public domain, pull the next one that is ??
 
 });
+
+
+// $.ajax(`${BASE_URL}/artworks/?fields=title,artist_title,date_display,medium_display`).then(function(results) {
+    // artData = results.data;
+    // console.log("Ajax Call", artData);
